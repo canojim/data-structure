79e188374197170096d32f48ef56208fdbaca7e8
canojim <cano.jim.cj@gmail.com>
2016-05-15 10:30:31 +0800

    test different variant of map

---
+++ src/per/ds/map/MapTest.java
@@ -0,0 +1,46 @@
+package per.ds.map;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
+
+public class MapTest {
+
+	public static void main(String[] args) {
+		TreeMap<String, String> tm = new TreeMap<String, String>();
+		tm.put("TreeMap key1", "value1");
+		tm.put("TreeMap key2", "value2");
+		tm.put("TreeMap key3", "value3");
+
+		for (String key: tm.keySet()) {
+			System.out.println("key: " + key);
+			// order (by key) is guarantee
+			// O(log n) insert and lookup
+		}
+
+		HashMap<String, String> hm = new HashMap<String, String>();
+		hm.put("HashMap key1", "value1");
+		hm.put("HashMap key2", "value2");
+		hm.put("HashMap key3", "value3");
+
+		for (String key: hm.keySet()) {
+			System.out.println("key: " + key);
+			//order is not guarantee
+			// O(1) insert and lookup (most of the time, when no collision)
+		}
+		
+		
+		//LinkedHashMap preserves the insertion order
+		LinkedHashMap<String, String> lhm = new LinkedHashMap<String, String>();
+		lhm.put("TreeMap key2", "value2");
+		lhm.put("TreeMap key1", "value1");
+		lhm.put("TreeMap key3", "value3");
+
+		for (String key: lhm.keySet()) {
+			System.out.println("key: " + key);
+			// order (by insertion) is guarantee
+			// O(log n) insert and lookup
+		}
+	}
+
+}
