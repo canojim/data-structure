3ae0322207070671fd09e4850795cfef4554dc75
canojim <cano.jim.cj@gmail.com>
2016-06-04 07:44:20 +0800

    string test

---
+++ src/per/ds/exercise/StringTest.java
@@ -0,0 +1,25 @@
+package per.ds.exercise;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class StringTest {
+	
+	@Test
+	public void testStringBuilder() {
+		String [] a = { "abc", "de", "fghi" };
+		Assert.assertEquals(concat(a), "abcdefghi");
+	}
+	
+	public String concat(String [] a) {
+		StringBuilder sb = new StringBuilder();
+		//replacement of StringBuffer if synchronization is not required.
+		//faster
+		
+		for (String s: a) {
+			sb.append(s);
+		}
+		
+		return sb.toString();
+	}
+}
